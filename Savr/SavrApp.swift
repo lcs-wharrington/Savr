010cec8106872a14746d928c124b4a284dbd92f9
William Harrington <wharrington@lcs.on.ca>
2022-05-11 10:07:57 -0400

    Initial Commit

---
+++ Savr/SavrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavrApp.swift
+//  Savr
+//
+//  Created by William Robert Harrington on 2022-05-11.
+//
+
+import SwiftUI
+
+@main
+struct SavrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
